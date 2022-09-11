4038a14422db914ab396e6f2602192060c1426b3
Danil Vinogradov <vinogradovspbu@gmail.com>
2022-09-11 23:31:47 +0300

    added working post example

--- server/index.js
+++ server/index.js
@@ -9,16 +9,22 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = 3000;
-const URL = "https://jsonplaceholder.typicode.com/";
+const URL = "https://jsonplaceholder.typicode.com";
+const TODOS_URI = "/todos";
 
-app.get('/todos', async (req, res) => {
-    const apiResponse = await axios.get(URL + "todos");
+app.get(TODOS_URI, async (req, res) => {
+    const apiResponse = await axios.get(URL + TODOS_URI);
     const data = apiResponse.data;
     res.send(data);
 })
 
-app.post('/todos', async (req, res) => {
-    const apiResponse = await axios.post(URL + "lksjdflkfsjd", { uuid: req.body.id });
+app.post(TODOS_URI, async (req, res) => {
+    const reqData = {
+        title: req.body.title || null,
+        body: req.body.body || null,
+        userId: req.body.userId || 1
+    }
+    const apiResponse = await axios.post(URL + TODOS_URI, reqData);
     const data = apiResponse.data;
     res.send(data);
 })
